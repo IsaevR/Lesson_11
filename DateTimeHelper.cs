c574dc947330c851975b739912535491fd9682d6
IsaevR <diplomatic.area@gmail.com>
2018-05-23 10:45:40 +0300

    firstcommit

---
+++ DateTimeHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_11
+{
+    static class DateTimeHelper
+    {
+        // метод расширение TakeDays(n) для DateTime
+        // которое возвращает IEnumerable<DateTime> содержащее
+        // n следующих за текущем днем дней.
+        public static IEnumerable<DateTime> TakeDays(this DateTime d, int n)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                DateTime tmp = d.AddDays(i + 1);
+
+                yield return tmp; // yield - итератор представляет метод, 
+                                  //в котором используется ключевое слово yield
+                                  //для перебора по коллекции или массиву
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_11
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            foreach (var d in DateTime.Now.TakeDays(3))
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
